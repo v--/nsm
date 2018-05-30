9d3d6269297d2158f57086eda6cc5dae3ed25a75
Ianis G. Vasilev <ianis@ivasilev.net>
2018-05-30 22:31:30 +0300

    Refine the naive discrete sampling

--- discrete_sampling/naive_discrete_sampling.m
+++ discrete_sampling/naive_discrete_sampling.m
@@ -1,15 +1,15 @@
-function[sample] = naive_discrete_sampling(n, prob_sequence)
+function[sample] = naive_discrete_sampling(n, pdf, first_value, next_value)
     sample = zeros(n, 1);
     index = 1;
 
     while index <= n
         u = rand(1);
-        m = 1;
-        y = prob_sequence(m);
+        m = first_value;
+        y = pdf(m);
 
         while u > y
-            m += 1;
-            y += prob_sequence(m);
+            m = next_value(m);
+            y += pdf(m);
         end
 
         sample(index) = m;
@@ -21,5 +21,11 @@ end
 %! % Binomial distribution
 %! n = 4;
 %! p = 1/2;
-%! sample = naive_discrete_sampling(100, @(m) binopdf(m - 1, n, p)) - 1;
+%! sample = naive_discrete_sampling(100, @(m) binopdf(m, n, p), 0, @(n) n+1);
 %! assert(is_uniform(binocdf(sample, n, p), 1e-3));
+
+%!test
+%! % Poisson distribution
+%! lambda = 2;
+%! sample = naive_discrete_sampling(100, @(m) poisspdf(m, lambda), 0, @(n) n+1);
+%! assert(is_uniform(poisscdf(sample, lambda), 1e-3));
